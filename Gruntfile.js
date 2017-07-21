0e80aaeb157c1b7f6f91501805779742179a79c2
Alexander Ferguson <x@echonotation.com>
2017-07-21 19:08:38 +0800

    Add js linting to check js for errors and proper formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,18 @@
 module.exports = function(grunt) {
 
+  grunt.initConfig({
+    jshint: {
+      all: ['Gruntfile.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
   // A very basic default task.
   grunt.registerTask('default', 'Grunt is running', function() {
     grunt.log.write('Grunt is running...').ok();
   });
+
+  grunt.registerTask('js', ['jshint']);
 
 };
