a4ea7c3dfe5e9fbd93838ddc67aafeb2b5fdd747
Alexander Ferguson <x@echonotation.com>
2017-07-21 18:51:14 +0800

    Add node and grunt dependency to repo

---
+++ Gruntfile.js
@@ -0,0 +1,8 @@
+module.exports = function(grunt) {
+
+  // A very basic default task.
+  grunt.registerTask('default', 'Grunt is running', function() {
+    grunt.log.write('Grunt is running...').ok();
+  });
+
+};
