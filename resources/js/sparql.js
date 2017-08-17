88f1d0a695a56f2ce4baf77ef9183e9dd3439b01
Alexander Ferguson <x@echonotation.com>
2017-08-17 12:45:23 +0800

    LINT javscript

--- resources/js/sparql.js
+++ resources/js/sparql.js
@@ -1,55 +1,58 @@
+/*globals
+    Rx, XMLHttpRequest, console, d3, MESSAGE, debug
+*/
 var SPARQL = (function(module) {
 
     module.ENDPOINT = "https://etytree-virtuoso.wmflabs.org/sparql";
 
     module.getXMLHttpRequest = function(url) {
-	return Rx.Observable.create(observer => {
+        return Rx.Observable.create(observer => {
             const req = new XMLHttpRequest();
             req.open('GET', url);
             req.overrideMimeType('application/sparql-results+json');
             req.onload = () => {
-		if (req.status === 200) {
+                if (req.status === 200) {
                     observer.next(req.responseText);
                     observer.complete();
-		} else {
+                } else {
                     observer.error(new Error(req.statusText));
-		}
+                }
             };
             req.onerror = () => {
-		observer.error(new Error('An error occured'));
+                observer.error(new Error('An error occured'));
             };
             req.setRequestHeader('Accept', 'application/json, text/javascript');
             req.send();
-	});
-    }
-    
+        });
+    };
+
     //function to slice up a big sparql query (that cannot be processed by virtuoso)                     
     // into a bunch of smaller queries in chunks of "chunk"                                             
     module.slicedQuery = function(myArray, queryFunction, chunk) {
-	var i, j, tmpArray, url, sources = [];
-	for (i = 0, j = myArray.length; i < j; i += chunk) {
+        var i, j, tmpArray, url, sources = [];
+        for (i = 0, j = myArray.length; i < j; i += chunk) {
             tmpArray = myArray.slice(i, i + chunk);
-	    //console.log(SPARQL.unionQuery(tmpArray, query));                   
+            //console.log(SPARQL.unionQuery(tmpArray, query));                   
             url = this.ENDPOINT + "?query=" + encodeURIComponent(this.unionQuery(tmpArray, queryFunction));
             if (debug) {
-		console.log(url);
+                console.log(url);
             }
-	    sources.push(this.getXMLHttpRequest(url));
-	}
-	const queryObservable = Rx.Observable.zip.apply(this, sources)
+            sources.push(this.getXMLHttpRequest(url));
+        }
+        const queryObservable = Rx.Observable.zip.apply(this, sources)
             .catch((err) => {
-		d3.select("#message").html(MESSAGE.serverError);
-		
-		//Return an empty Observable which gets collapsed in the output             
-		return Rx.Observable.empty();
+                d3.select("#message").html(MESSAGE.serverError);
+
+                //Return an empty Observable which gets collapsed in the output             
+                return Rx.Observable.empty();
             });
-	return queryObservable;
-    }
+        return queryObservable;
+    };
 
 
     module.disambiguationQuery = function(lemma) {
         var encodedWord = lemma.replace(/'/g, "\\\\'").replace("·", "%C2%B7");
-        var query = 
+        var query =
             "PREFIX dbetym: <http://etytree-virtuoso.wmflabs.org/dbnaryetymology#> " +
             "PREFIX dbnary: <http://kaiko.getalp.org/dbnary#> " +
             "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
@@ -65,14 +68,14 @@ var SPARQL = (function(module) {
             "        ?iri dbnary:describes  ?ee . " +
             "        ?ee rdf:type dbetym:EtymologyEntry . " +
             "    } " +
-	    "} ";
-        
+            "} ";
+
         return query;
     };
 
     //DEFINE QUERY TO GET LINKS, POS AND GLOSS           
-    module.lemmaQuery = function(iri) { 
-        var query = 
+    module.lemmaQuery = function(iri) {
+        var query =
             "PREFIX dbnary: <http://kaiko.getalp.org/dbnary#> " +
             "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
             "PREFIX lexinfo: <http://www.lexinfo.net/ontology/2.0/lexinfo#> " +
@@ -120,13 +123,13 @@ var SPARQL = (function(module) {
             "            ?val rdf:value ?def . " +
             "        } " +
             "    } " +
-	    "} ";
+            "} ";
         return query;
     };
 
-	//(related|equivalent){0,5}
+    //(related|equivalent){0,5}
     //DEFINE QUERIES TO PLOT GRAPH          
-    module.ancestorQuery = function(iri, queryDepth) {     
+    module.ancestorQuery = function(iri, queryDepth) {
         var query = "PREFIX dbetym: <http://etytree-virtuoso.wmflabs.org/dbnaryetymology#> ";
         if (queryDepth === 1) {
             query +=
@@ -137,28 +140,28 @@ var SPARQL = (function(module) {
                 "   ?eq dbetym:etymologicallyRelatedTo* ?ancestor2 .} " +
                 "} ";
         } else if (queryDepth === 2) {
-            query += 
+            query +=
                 "SELECT DISTINCT ?ancestor1 " +
                 "{ " +
                 "   <" + iri + "> dbetym:etymologicallyRelatedTo{0,5} ?ancestor1 . " +
-	        "} ";
-	}
-	return query;
+                "} ";
+        }
+        return query;
     };
 
-    module.descendantQuery = function(iri) { 
-        var query = 
+    module.descendantQuery = function(iri) {
+        var query =
             "SELECT DISTINCT ?descendant1 " + // ?descendant2",
             "{ " +
             "   ?descendant1 dbetym:etymologicallyRelatedTo{0,2} <" + iri + "> . " +
             //   "   OPTIONAL {?eq dbetym:etymologicallyEquivalentTo ?descendant1 . " +
             //  "   ?descendant2 dbetym:etymologicallyRelatedTo* ?eq .} " +
-	    "} ";
+            "} ";
         return query;
     };
 
-    module.propertyQuery = function(iri) { 
-        var query = 
+    module.propertyQuery = function(iri) {
+        var query =
             "SELECT DISTINCT ?s ?rel ?eq ?der " +
             "{           " +
             "   VALUES ?rel " +
@@ -169,15 +172,15 @@ var SPARQL = (function(module) {
             "   OPTIONAL { ?rel dbetym:etymologicallyEquivalentTo{0,6} ?eq . } " +
             "   OPTIONAL { ?s dbetym:etymologicallyDerivesFrom ?der . } " +
             //  "   FILTER NOT EXISTS { ?rel dbetym:etymologicallyDerivesFrom ?der2 . } "+
-	    "}";
+            "}";
         return query;
     };
 
-    module.unionQuery = function(iriArray, query) {
-        var query = 
+    module.unionQuery = function(iriArray, queryFunction) {
+        var query =
             "PREFIX dbetym: <http://etytree-virtuoso.wmflabs.org/dbnaryetymology#> " +
-	    "SELECT * WHERE {{ " +
-             iriArray.map(function(iri) { return query(iri); }).join("} UNION {") +
+            "SELECT * WHERE {{ " +
+            iriArray.map(function(iri) { return queryFunction(iri); }).join("} UNION {") +
             "}}";
         return query;
     };
