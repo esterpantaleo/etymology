61035c7a9cb352141b318c09987799aa7a02405e
Alexander Ferguson <x@echonotation.com>
2017-08-20 10:45:48 +0800

    wrap dagre in a module named GRAPH so that it is replaceable by other modules

--- resources/js/etytree.js
+++ resources/js/etytree.js
@@ -1,5 +1,5 @@
 /*globals
-    jQuery, d3, LOAD, console, window, document, SPARQL, drawDisambiguation, buildDisambiguationDAGRE, renderGraph, drawDAGRE, appendDefinitionTooltipOrDrawDAGRE, appendLanguageTagTextAndTooltip
+    jQuery, d3, LOAD, console, window, document, SPARQL, GRAPH
 */
 jQuery('document').ready(function($) {
     d3.select("#helpPopup").html(LOAD.HELP.intro);
@@ -37,35 +37,35 @@ jQuery('document').ready(function($) {
 
                 const source = SPARQL.getXMLHttpRequest(url);
                 source.subscribe(
-				            function(response) {
-				                if (response !== undefined && response !== null) { 
-					                  d3.select("#tree-overlay").remove();
-					                  d3.select("#tooltipPopup").style("display", "none");
-					 
-					                  var g = buildDisambiguationDAGRE(response);
-					                  if (null === g) { 
-					                      d3.select("#message").style("display", "inline").html(MESSAGE.notAvailable);
-					                  } else {
-					                      if (Object.keys(g.nodess).length > 1) {
-						                        d3.select("#helpPopup").html(LOAD.HELP.disambiguation);  
-						                        d3.select("#message").style("display", "inline").html("There are multiple words in the database. <br>Which word are you interested in?");
-						                        var inner = renderGraph(g, width, height);
-						                        appendLanguageTagTextAndTooltip(inner, g);
-						                        appendDefinitionTooltipOrDrawDAGRE(inner, g, width, height);
-						 
-						                        d3.selectAll(".edgePath").remove();
-					                      } else {    
-						                        var iri = Object.keys(g.nodess)[0];
-						                        drawDAGRE(iri, 2, width, height);   
-					                      }
-					                  }
-				                }
-				            },
-				            function(error){ 
-				                console.error(error);
-				                d3.select("#message").style("display", "inline").html(MESSAGE.notAvailable);
-				            },
-				            () => console.log('done disambiguation'));
+                    function(response) {
+                        if (response !== undefined && response !== null) {
+                            d3.select("#tree-overlay").remove();
+                            d3.select("#tooltipPopup").style("display", "none");
+
+                            var g = GRAPH.buildDisambiguationDAGRE(response);
+                            if (null === g) {
+                                d3.select("#message").style("display", "inline").html(LOAD.MESSAGE.notAvailable);
+                            } else {
+                                if (Object.keys(g.nodess).length > 1) {
+                                    d3.select("#helpPopup").html(LOAD.HELP.disambiguation);
+                                    d3.select("#message").style("display", "inline").html("There are multiple words in the database. <br>Which word are you interested in?");
+                                    var inner = GRAPH.renderGraph(g, width, height);
+                                    GRAPH.appendLanguageTagTextAndTooltip(inner, g);
+                                    GRAPH.appendDefinitionTooltipOrDrawDAGRE(inner, g, width, height);
+
+                                    d3.selectAll(".edgePath").remove();
+                                } else {
+                                    var iri = Object.keys(g.nodess)[0];
+                                    GRAPH.drawDAGRE(iri, 2, width, height);
+                                }
+                            }
+                        }
+                    },
+                    function(error) {
+                        console.error(error);
+                        d3.select("#message").style("display", "inline").html(LOAD.MESSAGE.notAvailable);
+                    },
+                    () => console.log('done disambiguation'));
             }
         }
     });
