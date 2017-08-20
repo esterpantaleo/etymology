c8569388890041556c132a8fb88e87a7ede79168
Alexander Ferguson <x@echonotation.com>
2017-08-20 11:54:42 +0800

    Wrap etytree in EtyTree object and run initialization as create and init functions

--- resources/js/etytree.js
+++ resources/js/etytree.js
@@ -1,72 +1,91 @@
 /*globals
-    jQuery, d3, LOAD, console, window, document, DB, GRAPH
+    jQuery, $, d3, LOAD, console, window, document, DB, GRAPH
 */
-jQuery('document').ready(function($) {
-    d3.select("#helpPopup").html(LOAD.HELP.intro);
+var EtyTree = {
+    create: function() {
+        var etytree = Object.create(this);
+        etytree.DB = DB;
+        etytree.GRAPH = GRAPH;
+        etytree.LOAD = LOAD;
+        return etytree;
+    },
+    init: function() {
+        var that = this;
+        d3.select("#helpPopup").html(that.LOAD.HELP.intro);
 
-    var div = d3.select("body").append("div")
-        .attr("data-role", "popup")
-        .attr("data-dismissible", "true")
-        .attr("id", "tooltipPopup")
-        .style("display", "none")
-        .attr("class", "ui-content tooltipDiv");
+        var div = d3.select("body").append("div")
+            .attr("data-role", "popup")
+            .attr("data-dismissible", "true")
+            .attr("id", "tooltipPopup")
+            .style("display", "none")
+            .attr("class", "ui-content tooltipDiv");
 
-    $(window).click(function() {
-        d3.select("#tooltipPopup")
-            .style("display", "none");
-    });
+        $(window).click(function() {
+            d3.select("#tooltipPopup")
+                .style("display", "none");
+        });
 
-    $('#tooltipPopup').click(function(event) {
-        event.stopPropagation();
-    });
+        $('#tooltipPopup').click(function(event) {
+            event.stopPropagation();
+        });
 
-    $('#tags').on("keypress click", function(e) {
-        if (e.which === 13 || e.type === 'click') {
-            var lemma = $(this).val(); //.replace("/", "!slash!");
+        $('#tags').on("keypress click", function(e) {
+            var tag = this;
+            if (e.which === 13 || e.type === 'click') {
+                var lemma = $(tag).val(); //.replace("/", "!slash!");
 
-            if (lemma) {
-                if (LOAD.settings.debug) {
-                    console.log("searching lemma in database");
-                }
-                var width = window.innerWidth,
-                    height = $(document).height() - $('#header').height();
-                var url = DB.ENDPOINT + "?query=" + encodeURIComponent(DB.disambiguationQuery(lemma));
-                if (LOAD.settings.debug) {
-                    console.log(url);
-                }
+                if (lemma) {
+                    if (that.LOAD.settings.debug) {
+                        console.log("searching lemma in database");
+                    }
+                    var width = window.innerWidth,
+                        height = $(document).height() - $('#header').height();
+                    var url = that.DB.ENDPOINT + "?query=" + encodeURIComponent(that.DB.disambiguationQuery(lemma));
+                    if (that.LOAD.settings.debug) {
+                        console.log(url);
+                    }
 
-                const source = DB.getXMLHttpRequest(url);
-                source.subscribe(
-                    function(response) {
-                        if (response !== undefined && response !== null) {
-                            d3.select("#tree-overlay").remove();
-                            d3.select("#tooltipPopup").style("display", "none");
+                    const source = that.DB.getXMLHttpRequest(url);
+                    source.subscribe(
+                        function(response) {
+                            if (response !== undefined && response !== null) {
+                                d3.select("#tree-overlay").remove();
+                                d3.select("#tooltipPopup").style("display", "none");
 
-                            var g = GRAPH.buildDisambiguationDAGRE(response);
-                            if (null === g) {
-                                d3.select("#message").style("display", "inline").html(LOAD.MESSAGE.notAvailable);
-                            } else {
-                                if (Object.keys(g.nodess).length > 1) {
-                                    d3.select("#helpPopup").html(LOAD.HELP.disambiguation);
-                                    d3.select("#message").style("display", "inline").html("There are multiple words in the database. <br>Which word are you interested in?");
-                                    var inner = GRAPH.renderGraph(g, width, height);
-                                    GRAPH.appendLanguageTagTextAndTooltip(inner, g);
-                                    GRAPH.appendDefinitionTooltipOrDrawDAGRE(inner, g, width, height);
-
-                                    d3.selectAll(".edgePath").remove();
+                                var g = that.GRAPH.buildDisambiguationDAGRE(response);
+                                if (null === g) {
+                                    d3.select("#message").style("display", "inline").html(that.LOAD.MESSAGE.notAvailable);
                                 } else {
-                                    var iri = Object.keys(g.nodess)[0];
-                                    GRAPH.drawDAGRE(iri, 2, width, height);
+                                    if (Object.keys(g.nodess).length > 1) {
+                                        d3.select("#helpPopup").html(that.LOAD.HELP.disambiguation);
+                                        d3.select("#message").style("display", "inline").html("There are multiple words in the database. <br>Which word are you interested in?");
+                                        var inner = that.GRAPH.renderGraph(g, width, height);
+                                        that.GRAPH.appendLanguageTagTextAndTooltip(inner, g);
+                                        that.GRAPH.appendDefinitionTooltipOrDrawDAGRE(inner, g, width, height);
+
+                                        d3.selectAll(".edgePath").remove();
+                                    } else {
+                                        var iri = Object.keys(g.nodess)[0];
+                                        that.GRAPH.drawDAGRE(iri, 2, width, height);
+                                    }
                                 }
                             }
-                        }
-                    },
-                    function(error) {
-                        console.error(error);
-                        d3.select("#message").style("display", "inline").html(LOAD.MESSAGE.notAvailable);
-                    },
-                    () => console.log('done disambiguation'));
+                        },
+                        function(error) {
+                            console.error(error);
+                            d3.select("#message").style("display", "inline").html(that.LOAD.MESSAGE.notAvailable);
+                        },
+                        () => console.log('done disambiguation'));
+                }
             }
-        }
-    });
+        });
+
+    }
+};
+
+var ety;
+
+jQuery('document').ready(function($) {
+    ety = EtyTree.create();
+    ety.init();
 });
